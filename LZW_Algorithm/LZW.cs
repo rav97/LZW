eb189f6c10129104aacdeb4fdb5894546f53c9b5
rav97 <35076475+rav97@users.noreply.github.com>
2019-11-20 19:56:19 +0100

    LZW

---
+++ LZW_Algorithm/LZW.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+static class LZW
+{
+    /*public static string Compress(string txt)
+    {
+        string compressed = String.Empty;
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+
+        for (int i = 0; i < txt.Length; i++)
+        {
+            if (!dict.ContainsKey(txt[i].ToString()))
+            {
+                dict.Add(txt[i].ToString(), dict.Count + 1);
+            }
+        }
+
+        foreach(var e in dict)
+        {
+            Console.WriteLine("{0}. {1}", e.Value, e.Key);
+        }
+
+        string c = txt[0].ToString();
+
+        for (int i = 1; i < txt.Length; i++)
+        {
+            string word = String.Concat(c, txt[i]);
+            if (dict.ContainsKey(word))
+            {
+                c = String.Concat(c, txt[i]);
+                //Console.WriteLine("{0} istnieje w slowniku", word);
+            }
+            else
+            {
+                compressed += dict[c];
+                dict.Add(word, dict.Count + 1);
+                c = String.Concat(txt[i]);
+                //Console.WriteLine("do slownika dodany jest ciag {0}", word);
+            }
+            if (i == txt.Length - 1)
+                compressed += dict[c];
+        }
+        return compressed;
+    }*/
+    public static List<UInt16> LZW_Encode(string txt)
+    {
+        //step 1
+        Dictionary<string, UInt16> D = new Dictionary<string, UInt16>();
+        UInt16 n = 0;
+
+        for(int i = 0; i < 256; i++)
+        {
+            D.Add(((char)i).ToString(), n);
+            n++;
+        }
+
+        //step 2
+        string c = txt[0].ToString();
+        List<UInt16> result = new List<UInt16>();
+
+        //step 3
+        for(int i = 1; i< txt.Length;i++)
+        {
+            string s = txt[i].ToString();
+
+            if (D.ContainsKey(String.Concat(c,s)))
+            {
+                c = c + s;
+            }
+            else
+            {
+                result.Add(D[c]);
+                D.Add(String.Concat(c, s), n);
+                n++;
+                c = s;
+            }
+        }
+        //step 4
+        result.Add(D[c]);
+
+        return result;
+    }
+    public static string LZW_Decode(List<UInt16> data)
+    {
+        //step 1
+        Dictionary<UInt16, string> D = new Dictionary<UInt16, string>();
+        UInt16 n = 0;
+
+        for (int i = 0; i < 256; i++)
+        {
+            D.Add(n, ((char)i).ToString());
+            n++;
+        }
+
+        //step 2
+        UInt16 pk = data[0];
+
+        //step 3
+        string result = D[pk];
+
+        //step 4
+
+        for(int i = 1; i < data.Count; i++)
+        {
+            string pc = D[pk];
+            UInt16 k = data[i];
+            if(D.ContainsKey(k))
+            {
+                D.Add(n, String.Concat(pc + D[k][0]));
+                n++;
+                result += D[k];
+            }
+            else
+            {
+                D.Add(n, String.Concat(pc + pc[0]));
+                n++;
+                result += String.Concat(pc + pc[0]);
+            }
+            pk = k;
+        }
+        return result;
+    }
+}
--- LZW_Algorithm/Program.cs
+++ LZW_Algorithm/Program.cs
@@ -1,15 +1,128 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LZW_Algorithm
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string inputFilepath = @"..\..\inputfile.txt";
+        string outputFilepath = @"..\..\compressed.bin";
+        string outputDecompressed = @"..\..\decompressed.txt";
+
+        string inputText = File.ReadAllText(inputFilepath, Encoding.ASCII);
+        List<UInt16> compressed = LZW.LZW_Encode(inputText);
+
+        byte[] bytes = ListToBytes(compressed);
+        File.WriteAllBytes(outputFilepath, bytes);
+
+        byte[] readBytes = File.ReadAllBytes(outputFilepath);
+        List<UInt16> readList = BytesToList(readBytes);
+
+        string decoded = LZW.LZW_Decode(readList);
+
+        float l1 = (float)inputText.Length, l2 = (float)bytes.Length;
+        Console.WriteLine("Plain text length: {0}", l1);
+        Entropy(inputText);
+        Console.WriteLine("Compressed text length: {0}", l2);
+        Entropy(compressed);
+        float compression = 100.0f * ((l1 - l2) / l1);
+        Console.WriteLine("Compression rate: {0}", compression);
+
+        File.WriteAllText(outputDecompressed, decoded);
+    }
+    public static byte[] ListToBytes(List<UInt16> list)
+    {
+        byte[] bytes = new byte[list.Count * 2];
+
+        int i = 0;
+
+        foreach (UInt16 e in list)
         {
+            byte[] b = BitConverter.GetBytes(e);
+            bytes[i] = b[0];
+            bytes[i + 1] = b[1];
+
+            i += 2;
         }
+        return bytes;
+    }
+    public static List<UInt16> BytesToList(byte[] bytes)
+    {
+        List<UInt16> list = new List<UInt16>(bytes.Length / 2);
+
+        for(int i = 0; i < bytes.Length; i+=2)
+        {
+            list.Add(BitConverter.ToUInt16(bytes, i));
+        }
+
+        return list;
+    }
+    public static void Entropy(string txt)
+    {
+        Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+        for(int i=0;i<txt.Length;i++)
+        {
+            if(charCount.ContainsKey(txt[i]))
+            {
+                charCount[txt[i]] += 1;
+            }
+            else
+            {
+                charCount[txt[i]] = 1;
+            }
+        }
+        print(charCount, (float)txt.Length);
+    }
+    public static void Entropy(List<UInt16> list)
+    {
+        Dictionary<UInt16, int> charCount = new Dictionary<UInt16, int>();
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (charCount.ContainsKey(list[i]))
+            {
+                charCount[list[i]] += 1;
+            }
+            else
+            {
+                charCount[list[i]] = 1;
+            }
+        }
+        print(charCount, (float)list.Count);
+    }
+    public static void print(Dictionary<char, int> dict, float size)
+    {
+        float percent;
+        double entropy = 0;
+        dict = dict.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+        foreach(KeyValuePair<char, int> kvp in dict)
+        {
+            percent = ((float)dict[kvp.Key] / size);
+            float p1 = 1 / percent;
+            entropy += percent * Math.Log(p1, 2.0d);
+            int byteCount = Encoding.ASCII.GetByteCount(kvp.Key.ToString());
+            //Console.WriteLine("{0}\t{1}\t{2}\t{3}", kvp.Key, byteCount, dict[kvp.Key], Math.Round(percent * 100, 2));
+        }
+        Console.WriteLine("Entropy: {0}", entropy);
+    }
+    public static void print(Dictionary<UInt16, int> dict, float size)
+    {
+        float percent;
+        double entropy = 0;
+        dict = dict.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+        foreach (KeyValuePair<UInt16, int> kvp in dict)
+        {
+            percent = ((float)dict[kvp.Key] / size);
+            float p1 = 1 / percent;
+            entropy += percent * Math.Log(p1, 2.0d);
+            int byteCount = 8;
+            //Console.WriteLine("{0}\t{1}\t{2}\t{3}", kvp.Key, byteCount, dict[kvp.Key], Math.Round(percent * 100, 2));
+        }
+        Console.WriteLine("Entropy: {0}", entropy);
     }
 }
